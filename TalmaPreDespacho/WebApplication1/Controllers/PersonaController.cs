7a9dac0f73e58b8382383379e3e7d3603ec29360
Allan Davis Grey Ferrari <greyferrari@gmail.com>
2016-11-12 21:48:00 -0500

    Add ApiRestful

---
+++ TalmaPreDespacho/WebApplication1/Controllers/PersonaController.cs
@@ -0,0 +1,96 @@
+﻿namespace WebApplication1.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    /// <summary>
+    /// PersonaController
+    /// </summary>
+    public class PersonaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult getPerson()
+        {
+            try
+            {
+                List<Person> list = new List<Person>();
+                //list = BL.traerDatos();
+                //object obj1 = new { id = "1" };
+                //object obj2 = new { id = "2" };
+
+                list.Add(new Person { id= "1"});
+                list.Add(new Person { id = "2" });
+                //list.Add(obj2);
+
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult getPersonById(string id)
+        {
+            try
+            {
+                return Ok(new Person { id = "1" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult insertPerson([FromBody] Person person)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult updatePerson([FromBody] Person person, [FromUri] string id)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult deletePerson([FromUri] string id)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+
+    public class Person
+    {
+        public string id { get; set; }
+    }
+}
---
+++ TalmaPreDespacho/WebApplication1/Controllers/beUsuarioController.cs
@@ -0,0 +1,104 @@
+﻿namespace WebApplication1.Controllers
+{
+    using EsquemaEntidades.Perfiles;
+    using LogicaNegocio.Seguridad;
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    /// <summary>
+    /// PersonaController
+    /// </summary>
+    public class beUsuarioController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult getUsuarios()
+        {
+            try
+            {
+                blUsuario bl = new blUsuario();
+                List<beUsuario> ocol = bl.GetUsuariosAll();
+                return Ok(ocol);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult getUsuario([FromUri] string Codigo)
+        {
+            try
+            {
+                blUsuario bl = new blUsuario();
+                beUsuario be = bl.GetUsuariobyCodigo(Codigo.ToString());
+                return Ok(be);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult getUsuarioById(string Codigo)
+        {
+            try
+            {
+                blUsuario bl = new blUsuario();
+                beUsuario be = bl.GetUsuariobyCodigo(Codigo.ToString());
+                return Ok(be);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult insertUsuario([FromBody] beUsuario usuario)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult updateUsuario([FromBody] beUsuario usuario, [FromUri] string id)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult deleteUsuario([FromUri] string id)
+        {
+            try
+            {
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
